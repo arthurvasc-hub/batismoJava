adc6bbfa2868d34b9e240dbf2afcfe25ed0196b9
arthurvasc <arthurvasc2@gmail.com>
2025-01-14 20:41:13 -0300

    Estudo batismo java

---
+++ src/main/java/LacosRepeticao/LacosDeRepeticao.java
@@ -0,0 +1,23 @@
+package LacosRepeticao;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+        /*
+        * Laços de repetição: Vão repetir infinitamente ou ate atingir o parametro desejado
+        * WHILE & FOR
+        * */
+
+        int numeroDeClones = 0;
+        int numeroMaximoDeClones = 50;
+
+       /* while(numeroDeClones < numeroMaximoDeClones) {
+            numeroDeClones++;
+            System.out.println("O naruto criou um clone das sombras " + numeroDeClones);
+        }*/
+
+        for (int i = 1; i <= numeroMaximoDeClones; i++) {
+            System.out.println("O naruto criou um clone das sombras " + i);
+
+        }
+    }
+}
