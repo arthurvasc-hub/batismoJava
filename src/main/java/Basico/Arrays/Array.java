3594757a84f9f030d4dcc993dfdd0f199b5eb5eb
arthurvasc <arthurvasc2@gmail.com>
2025-01-15 15:22:42 -0300

    Estudos sobre inicizalição de objetos na memória

--- src/main/java/Basico/Arrays/Array.java
+++ src/main/java/Basico/Arrays/Array.java
@@ -5,16 +5,36 @@ import Basico.Desafio01.Ninjas;
 public class Array {
     public static void main(String[] args) {
 
-        String[] ninjas  = new String[5];
+        // Arrays são tipo de referência
+        // arrays inicializam como null
+        String[] ninjas  = new String[6];
         ninjas[0] = "Naruto Uzumaki";
         ninjas[1] = "Sasuke Uchiha";
         ninjas[2] = "Sakura Haruno";
         ninjas[3] = "Hinata Hyuga";
         ninjas[4] = "Kimimaro";
 
-        System.out.println("Primeiro ninja registrado no torenio foi: " + ninjas[0]);
-        System.out.println("Nome dos ninjas registrados : ");
-        for(String x:ninjas)
-            System.out.println(x);
+        /*
+            System.out.println("Primeiro ninja registrado no torenio foi: " + ninjas[0]);
+            System.out.println("Nome dos ninjas registrados : ");
+            for(String x:ninjas)
+                System.out.println(x);
+            */
+
+        System.out.println(ninjas[5]);
+
+        // int inicializam como 0
+        int[] idade = new int[2];
+            idade[0] = 10;
+            System.out.println(idade[1]);
+
+        // boolean inicializam como false
+        boolean[] verdadeiroOuFalso = new boolean[1];
+            System.out.println(verdadeiroOuFalso[0]);
+
+        // double inicializam como 0.0
+        double[] flutuante = new double[2];
+        flutuante[0] = 10;
+            System.out.println(flutuante[1]);
     }
 }
