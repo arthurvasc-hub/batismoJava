ef615dd829ef7c36d36d50aac37063ae75430d78
arthurvasc <arthurvasc2@gmail.com>
2025-01-30 23:41:22 -0300

    Entendo mais sobre record em java

--- src/main/java/Intermediario/Aulas/Record/NinjaRecord.java
+++ src/main/java/Intermediario/Aulas/Record/NinjaRecord.java
@@ -2,6 +2,7 @@ package Intermediario.Aulas.Record;
 
 public record NinjaRecord(String nome, int idade, double altura) {
     //Não preciso criar Getters, toString(), hashCode(), Construtores, pois ja está embutido no record.
+    //Não tenho SETTERS pois o Record ja configura os atributos como FINAL.
     //Posso criar métodos como uma classe normal
     //Reduzindo linhas de código.
 
